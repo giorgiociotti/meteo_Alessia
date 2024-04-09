ed2ec32610e89cb5e1ef0efc8145704127d70cbc
Giorgio <gciotti29@gmail.com>
2024-04-09 13:05:00 +0200

    screenshot meteo

--- src/test/java/org/example/TestMeteo.java
+++ src/test/java/org/example/TestMeteo.java
@@ -3,10 +3,9 @@ package org.example;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
@@ -19,7 +18,12 @@ import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 
 
@@ -61,6 +65,41 @@ public class TestMeteo extends Listener {
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
+    }
+    @Test(priority = 1)
+    public void login(){
+    driver.get("https://www.ilmeteo.it/meteo.php?citta_input=Roma");
+    }
+    @Test(priority = 2)
+    public void meteo() throws IOException {
+        WebElement tbody= find(tableTbody);
+        int x = tbody.getLocation().getX();
+        int y = tbody.getLocation().getY();
+        int width = tbody.getSize().getWidth();
+        int height = tbody.getSize().getHeight();
+        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         
+        // Ritaglia l'immagine per includere solo l'elemento desiderato
+        BufferedImage fullImg = ImageIO.read(screenshot);
+        BufferedImage elementScreenshot = fullImg.getSubimage(x, y, width, height);
+        ImageIO.write(elementScreenshot, "png", screenshot);
+        Date currentdate = new Date();
+        String screenshotfilename = currentdate.toString().replace(" ", "-").replace(":", "-");
+        // Salva lo screenshot come file
+        FileUtils.copyFile(screenshot, new File(".//screenshot//" + screenshotfilename + ".png"));
+    }
+    
+    @AfterTest
+    public void fine() {
+        
+        driver.quit();
+        
+        try {
+            softAssert.assertAll();
+            System.out.println(assertSucces);
+        } catch (AssertionError ae) {
+            System.out.println(ae);
+            
+        }
     }
 }
